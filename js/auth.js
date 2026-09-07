f57980e28a901c9e2b29d8f70f726be5e17cdb7b
razeen-secops <razeenbagwan@gmail.com>
2026-09-08 00:01:14 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,13 +2,15 @@ const auth0Client = await auth0.createAuth0Client({
   domain: "dev-fjph8ocq277vhnpf.us.auth0.com",
   clientId: "5UFeiM57b1aPOXfuA8oclA7GVSXw58Id",
   authorizationParams: {
-    redirect_uri: window.location.origin
+    redirect_uri: window.location.origin + "/login.html"
   }
 });
 
+// Handle Auth0 redirect
 if (window.location.search.includes("code=")) {
   await auth0Client.handleRedirectCallback();
-  window.history.replaceState({}, document.title, "/");
+  window.history.replaceState({}, document.title, "/login.html");
+  alert("Login successful!");
 }
 
 document.getElementById("login").addEventListener("click", async () => {
